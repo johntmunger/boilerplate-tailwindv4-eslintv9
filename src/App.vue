880248bd0ab0e6aa68af52a5f2b513c4555048ba
johntmunger <johntmunger@gmail.com>
2025-11-18 14:26:22 -0700

    wrap some styles with tailwind for app interface

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,45 @@
-<script setup></script>
+<script setup>
+import TheHeader from "./components/TheHeader.vue";
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-    Click Me
-  </button>
+  <main class="container m-auto p-10">
+    <TheHeader />
+    <form id="entry-form" class="entry-form">
+      <textarea placeholder="Who is your friend?"></textarea>
+      <div class="entry-form-footer">
+        <span>Currenly you have no friends</span>
+        <button>
+          Add a friend
+          <span class="icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                style="fill: transparent"
+              />
+            </svg>
+          </span>
+        </button>
+      </div>
+    </form>
+    <ul></ul>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.entry-form textarea {
+  height: 4rem;
+}
+button {
+  background-color: steelblue;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <h1 class="text-5xl flex items-center">
+    <strong>My Friendly App</strong>
+  </h1>
+</template>
+
+<style scoped></style>
