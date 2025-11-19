85254603f325df5acb23aa47d3d946a2608710b2
johntmunger <johntmunger@gmail.com>
2025-11-18 20:55:06 -0700

    easy validation to check if form empty

--- src/components/FormEntry.vue
+++ src/components/FormEntry.vue
@@ -13,24 +13,23 @@ function addFriend() {
 </script>
 
 <template>
-  <form>
-    <textarea v-model="newFriend" placeholder="Enter friend's name" />
-    <button type="button" @click="addFriend">Add Friend</button>
-  </form>
-  <div>
-    <h2>Your Friends:</h2>
-    <ul>
-      <li v-for="(name, index) in friends.names" :key="index">{{ name }}</li>
-    </ul>
-  </div>
   <form id="entry-form" class="entry-form">
-    <textarea placeholder="Who is your friend?"></textarea>
+    <textarea placeholder="Who is your friend?" v-model="newFriend" name="fname" required>
+></textarea
+    >
     <div class="entry-form-footer">
-      <span>Currenly you have no friends</span>
+      <span
+        >You have
+        <span v-if="friends.names.length === 0">no friends</span>
+        <span v-else-if="friends.names.length === 1">{{ friends.names.length }} friend!</span>
+        <span v-else> {{ friends.names.length }} friends!</span>
+      </span>
 
       <button
         type="button"
         class="active:shadow-inner active:scale-95 transition duration-150 ease-in-out cursor-pointer"
+        @click="addFriend"
+        :disabled="newFriend.length === 0"
       >
         Add a friend
         <span class="icon">
@@ -53,11 +52,38 @@ function addFriend() {
       </button>
     </div>
   </form>
+
+  <ul>
+    <li class="entry-form margins" v-for="(name, index) in friends.names" :key="index">
+      <span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z"
+            style="fill: transparent"
+          />
+        </svg>
+      </span>
+      <span>{{ name }}</span>
+    </li>
+  </ul>
 </template>
 
 <style scoped>
 .entry-form textarea {
   height: 4rem;
+}
+.entry-form.margins {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
 }
 button {
   background-color: steelblue;
